b7365114aa49529d5c6df99238a1c3387fd12019
kbhujbal <kbhujbal@usc.edu>
2021-01-26 12:00:00 -0800

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+GeoIntel - Satellite Military Asset Classification System
+==========================================================
+
+A SAHI-powered reconnaissance system for detecting small military vehicles
+(tanks, trucks, cargo) in high-resolution satellite imagery.
+"""
+
+__version__ = "1.0.0"
+__author__ = "GeoIntel Team"
